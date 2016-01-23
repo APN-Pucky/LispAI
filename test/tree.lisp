0ae521cfe31cc5eacd8e72ea1ec785fc8a213772
APN-Pucky <APN-Pucky@users.noreply.github.com>
2016-01-23 22:02:49 +0100

    more ops

--- test/tree.lisp
+++ test/tree.lisp
@@ -5,11 +5,17 @@
 (setf (gethash '- ops) 2)
 (setf (gethash '* ops) 2)
 (setf (gethash '/ ops) 2)
-;;;(setf (gethash 'getm ops) 2)
-(setf (gethash 'sin ops) 1)
+;;;(setf (gethash 'getms ops) 2)
+;;;(setf (gethash 'sin ops) 1)
+(setf (gethash 'abs ops) 1)
+(setf (gethash 'mod ops) 2)
+;;;(setf (gethash 'expt ops) 2)
 
 (defun hash-keys (hash-table)
-  (loop for key being the hash-keys of hash-table collect key))
+  	(loop for key being the hash-keys of hash-table collect key))
+
+(defun getms (x y)
+	(getm (abs (mod x 4)) (abs (mod y 4))))
 
 (defun make-tree-node ()
 	(if (< (random 1.0) 0.5)
@@ -19,7 +25,6 @@
 			(dotimes (r (gethash key ops))
 				(push (make-tree-node) array))
 			(push key array)
-			;;;(print array)
 			(return-from make-tree-node array)
 			)))
 
